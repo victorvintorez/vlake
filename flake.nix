c00a2c8f156c0f0f7efacdb0e58a6deb34689a24
victorvintorez <victor@vintorez.dev>
2025-04-03 18:27:25 +0100

    add lib

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "Victor Vintorez's Nix Flake";
+
+  outputs = { nixpkgs, ... }@inputs:
+    let
+      lib = import ./lib { inherit nixpkgs; };
+
+      nixosConfigurations = import ./hosts { inherit inputs lib; };
+
+      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+    in {
+      inherit nixosConfigurations;
+
+      packages = forEachSystem (_:
+        {
+          # Something Here
+        });
+    };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    agenix.url = "github:ryantm/agenix";
   };
 }
---
+++ lib/default.nix
@@ -0,0 +1,14 @@
+{
+  nixpkgs,
+  ...
+}: nixpkgs.lib.extend (
+  _final: prev: let
+    inherit (prev) mkIf;
+  in {
+    vlake = {
+      modules = {
+        mkIfNotNull = x: mkIf (x != null);
+      };
+    };
+  }
+)
