be4feb0f964b3f98e509eb673aeace533cbec66e
victorvintorez <victor@vintorez.dev>
2025-07-03 00:16:43 +0100

    Have both relative and absolute paths to identities

--- modules/system/agenix.nix
+++ modules/system/agenix.nix
@@ -18,7 +18,9 @@ in {
         } ${pkgs.age}/bin/age";
       identityPaths = [
         "${flake}/secrets/identities/yubikey-supernova.txt"
-        ".${flake}/secrets/identities/yubikey-wormhole.txt"
+        "${flake}/secrets/identities/yubikey-wormhole.txt"
+        "../../secrets/identities/yubikey-supernova.txt" # Just in case flake is not in flake location (such as during install)
+        "../../secrets/identities/yubikey-wormhole.txt" # Just in case flake is not in flake location (such as during install)
       ] ++ map (e: e.path)
         (filter (e: e.type == "ed25519") config.services.openssh.hostKeys);
       secrets = { defaultPassword.file = ../../secrets/defaultPassword.age; };
